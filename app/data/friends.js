7fd0909f6ff78039a0d97beaffc7a973d51a0c80
Emmy Sudhi <suchaisawat@gmail.com>
2018-02-14 23:52:35 -0600

    update the css

---
+++ app/data/friends.js
@@ -0,0 +1,40 @@
+var friends = [
+    {
+        "name": "James Richards",
+        "photo": "http://www.mulierchile.com/man/man-059.jpg",
+        "scores": [4, 1, 4, 2, 5, 1, 2, 3, 4, 4]
+   },
+    {
+        "name": "William Joseph",
+        "photo": "https://images-na.ssl-images-amazon.com/images/M/MV5BMTc3MzY3MjQ3OV5BMl5BanBnXkFtZTcwODI3NjQxMw@@._V1_UY1200_CR115,0,630,1200_AL_.jpg",
+        "scores": [4, 3, 5, 5, 3, 3, 2, 4, 3, 2]
+   },
+    {
+        "name": "Donald Therana",
+        "photo": "https://images-na.ssl-images-amazon.com/images/M/MV5BMTc2MTM2MjQ3OF5BMl5BanBnXkFtZTcwNDU4NDIxOQ@@._V1_UY1200_CR85,0,630,1200_AL_.jpg",
+        "scores": [5, 2, 2, 2, 4, 1, 3, 2, 5, 5]
+   },
+    {
+        "name": "Paul Bruces",
+        "photo": "https://img.bleacherreport.net/cms/media/image/55/c8/7b/39/1de0/4376/9f6c/35e0c8609815/crop_exact_BFM_L1_3x2_NoLogo.jpg?h=1999&q=90&w=3000",
+        "scores": [1, 4, 4, 2, 5, 1, 2, 2, 3, 3]
+   },
+    {
+        "name": "Jamie Dornan",
+        "photo": "https://images-na.ssl-images-amazon.com/images/M/MV5BMjQwMDAzMjAyM15BMl5BanBnXkFtZTgwMjAxMzc0MjE@._V1_UY1200_CR156,0,630,1200_AL_.jpg",
+        "scores": [4, 1, 4, 4, 5, 3, 5, 1, 1, 1]
+	},
+    {
+        "name": "Nithin Scott",
+        "photo": "https://www.hypnotherapykent.co.uk/wp/wp-content/uploads/job-interview-suit-man-main_Full2.jpg",
+        "scores": [3, 3, 4, 1, 4, 3, 5, 3, 5, 4]
+   },
+    {
+        "name": "Randy Beverly",
+        "photo": "http://www.newmanexports.com/images/gallery-img-5.jpg",
+        "scores": [4, 4, 2, 3, 2, 2, 3, 2, 4, 5]
+	}
+
+]
+
+module.exports = friends;
